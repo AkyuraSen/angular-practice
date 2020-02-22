d4429fcf3d213bd6395cd481391c061138ba0c3d
AkyuraSen <oyhz55@gmail.com>
2020-02-22 23:01:10 +0800

    5 files modified, 5 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotify() {
+    window.alert('Alright, we\'ll let you know when it\'s on sale!');
+  }
 }
 
 
